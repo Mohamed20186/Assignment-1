65d1a6d591e0aada73ecb08b5817607a1c6c619f
mohamed ahmed <m.ahmed20201863@gmail.com>
2026-08-28 21:41:18 +0300

    add function to merge two arrays and log the result

--- Part1_5.js
+++ Part1_5.js
@@ -2,3 +2,11 @@ let arryOne = [1,2,3];
 let arryTwo = [4,5,6];
 let mergedArray=[...arryOne,...arryTwo];
 console.log(mergedArray);
+
+
+//...... OR ........
+
+function mergeArrays(arr1, arr2) {
+    return [...arr1, ...arr2];
+}
+console.log(mergeArrays([1, 2, 3], [4, 5, 6]));
