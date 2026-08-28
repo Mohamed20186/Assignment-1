88a80f1a5b48cfec77c5c4f9dcd17e71aa9bc423
mohamed ahmed <m.ahmed20201863@gmail.com>
2026-08-28 21:30:55 +0300

    add initial implementation of Part1_1.js and Part1_2.js with basic functionality

---
+++ Part1_1.js
@@ -0,0 +1,3 @@
+let num = "123";
+let sum = Number(num) + 7;
+console.log(sum);
---
+++ Part1_2.js
@@ -0,0 +1,4 @@
+function falsyChecker(value) {
+    return value ? "Valid" : "Invalid";
+}
+console.log(falsyChecker(1)); 
