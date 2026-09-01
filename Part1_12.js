1b1677d7a10933c0198af9c55fbc89652b7dc7c1
mohamed ahmed <m.ahmed20201863@gmail.com>
2026-09-01 20:01:27 +0300

    add implementation of Part1_12.js for delaySuccess function with object parameter

---
+++ Part1_12.js
@@ -0,0 +1,9 @@
+function delaySuccess(success) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(success);
+        }, 3000);
+    });
+}
+
+console.log(delaySuccess('Success!').then(result => console.log(result)));
